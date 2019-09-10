fc8ca761f1e0a1dcb9eac57759a7778410e15e9c
Nikole <nh131215@gmail.com>
2019-09-10 15:55:57 -0400

    user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,25 @@ class UsersController < ApplicationController
         users = User.all 
         render json: users
     end
+
+    def new 
+        user = User.new
+    end
+
+    def create 
+        user = User.new(user_params)
+
+        if user.save 
+            render json: user
+        else 
+            render json: {message: 'invalid', errors: user.errors.full_messages}, status: 406
+        end
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
 end
