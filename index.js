d853201f23c1d6dcb55ecc07c89b5e78604a2fbf
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-09-12 09:29:53 -0400

    Flow

--- index.js
+++ index.js
@@ -3,165 +3,172 @@ document.addEventListener("DOMContentLoaded", function(){
     const hotelsURL = "http://localhost:3000/hotels"
     const booksURL = "http://localhost:3000/bookings"
 
+    // const mainContainer = document.querySelector("#main")
+    const titleContainer = document.querySelector("#title")
+    const contentContainer = document.querySelector("#content")
+    const messageContainer = document.querySelector("#message")
 
-    const userForm = document.querySelector("#user")
-    const userCont = document.querySelector("#user-container")
+    // let seeHotelsButton
 
-    let userId;
+    function renderUserForm(){
+        titleContainer.insertAdjacentHTML("beforeend", `
+            <h4>Welcome to Hotels Explorer</h4>
+        `)
+        contentContainer.insertAdjacentHTML("beforeend", `
+            <form id="user">
+                <label for="name">Enter your name to create profile: </label><br>
+                <input type="text" name="name"><br>
+                <input type="submit" id="submit" data-action="submit" class="submit button">
+            </form>
+        `)
 
-    userForm.addEventListener("submit", function (e){
-        e.preventDefault()
+        const userForm = document.querySelector("#user")
+        let userId;
 
-        // console.log("SUBMIT!")
+        userForm.addEventListener("submit", function (e){
+            e.preventDefault()
 
-        const userName = e.target.name.value
-        const userLoc = e.target.location.value
+            const userName = e.target.name.value
+            // const userLoc = e.target.location.value
 
-        // debugger
+            const formData = {
+                name: userName,
+                // search: userLoc
+            }
 
-        const formData = {
-            name: userName,
-            search: userLoc
-        }
+            console.log(formData)
 
-        console.log(formData)
+            fetch(usersURL, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accepts": "application/json"
+                },
+                body: JSON.stringify(formData)
+            })
+            .then(res => res.json())
+            .then(userInfo => {
+                console.log(userInfo)
+                userId = userInfo.id
+                // search = userInfo.search
+                titleContainer.innerHTML = `
+                    <h4>Hello, ${userInfo.name}</h4>
+                `
+            })
 
-        fetch(usersURL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accepts": "application/json"
-            },
-            body: JSON.stringify(formData)
-        })
-        .then(res => res.json())
-        .then(userInfo => {
-            console.log(userInfo)
-            userId = userInfo.id
-            userCont.innerHTML = `
-                <h1>Welcome, ${userInfo.name}</h1>
-                <div id="books">
-                    <h2> You don't have any hotels booked.</h2>
-                    You're going to ${userInfo.search} <br>
-                    <button id="my-books">My Hotels</button>
-                </div>
-            `
-        })
-        
-
-    })
-
-    
-    const bookButton = document.createElement("BUTTON")
-    bookButton.innerHTML = "Book a hotel"
-
-    document.body.appendChild(bookButton)
-    
-    
-    // document.getElementById("book-hotel")
-    
-    bookButton.addEventListener("click", function(){
-        console.log("CLICK")
-
-        fetch(hotelsURL)
-        .then(res => res.json())
-        .then(data => {
-            hotel = data
-            console.log(hotel)
-            const hotelsArray = hotel.businesses.slice(0, 10)
-            console.log(hotelsArray)
-
-            for(let i = 0; i < hotelsArray.length; i++){
-                const name = hotelsArray[i].name
-                const img = hotelsArray[i].image_url
-                const rating = hotelsArray[i].rating
-                const price = hotelsArray[i].price
-                const phone = hotelsArray[i].phone
-                const address1 = hotelsArray[i].location.display_address[0]
-                const address2 = hotelsArray[i].location.display_address[1]
-                const id = hotelsArray[i].id
-
-                // console.log(name, img, rating, price, phone, address1, address2, id)
-
-                const allHotels = document.querySelector("#all-hotels")
-
-                // append to page
-                const str = `
-                    <div class="hotels">
-                        <div class="hotel" id="hotel">
-                            <h4>${name}</h4>
-                            <img class="i" src="${img}"/>
-                            <div>Phone: ${phone}</div>
-                            <div id="a1">${address1}</div>
-                            <div id="a2">${address2}</div>
-                            <div>Rating: ${rating}</div> 
-                            <div>Price: ${price}</div>
-
-                            <button id="button-${id}">BOOK</button>
-                        </div>
+            contentContainer.innerHTML = `
+                    <div id="books">
+                        <h5> You don't have any hotels booked.</h5>
+                        <button id="see-hotels">See Hotels</button>
+                        <button id="my-hotels">My Hotels</button>
                     </div>
                 `
-                allHotels.insertAdjacentHTML("beforeend", str)
-
-                // change display 
-                // change user cont to display  nonne
-
-
-                // event delegation on allHotels
-                // or
-
-
-
-                // const bookBtnArr = document.querySelectorAll("#book")
-
-                const bookBtn = document.querySelector(`#button-${id}`)
                 
+            const seeHotelsButton = document.querySelector("#see-hotels")
 
-                // bookBtnArr.forEach(function(btn){
+            seeHotelsButton.addEventListener("click", function(e){
+                console.log("CLICK")
+            
+                fetch(hotelsURL)
+                .then(res => res.json())
+                .then(data => {
+                    hotel = data
+                    console.log(hotel)
+                    const hotelsArray = hotel.businesses.slice(0, 10)
+                    console.log(hotelsArray)
+                    titleContainer.innerHTML = `
+                        <h4>Hotels in New York</h4>
+                    `
+                    contentContainer.innerHTML = `
+                        <div id="books">
+                            
+                        </div>
+                    `
+
+                    for(let i = 0; i < hotelsArray.length; i++){
+                        const name = hotelsArray[i].name
+                        const img = hotelsArray[i].image_url
+                        const rating = hotelsArray[i].rating
+                        const price = hotelsArray[i].price
+                        const phone = hotelsArray[i].phone
+                        const address1 = hotelsArray[i].location.display_address[0]
+                        const address2 = hotelsArray[i].location.display_address[1]
+                        const id = hotelsArray[i].id
 
 
+                        const str = `
+                            <div class="hotels">
+                                <div class="hotel" id="hotel">
+                                    <h4>${name}</h4>
+                                    <img class="i" src="${img}"/>
+                                    <div>Phone: ${phone}</div>
+                                    <div id="a1">${address1}</div>
+                                    <div id="a2">${address2}</div>
+                                    <div>Rating: ${rating}</div> 
+                                    <div>Price: ${price}</div>
 
-                bookBtn.addEventListener("click", function(e){
-                    // console.log("ALL CLICK") 
-                    
+                                    <button id="button-${id}">Add to my favorites</button>
+                                </div>
+                            </div>
+                        `
+                        contentContainer.insertAdjacentHTML("beforeend", str)
 
-                    const address = address1 + " " + address2
 
-                    const hotelData = {
-                        hotelName: name,
-                        address: address,
-                        image: img,
-                        user_id: userId
+                        
+                        const bookBtn = document.querySelector(`#button-${id}`)
+
+                        bookBtn.addEventListener("click", function(e){
+                            // console.log("ALL CLICK") 
+
+                            messageContainer.innerHTML = `
+                                <div id="books">
+                                    <p>${name} was added</p>
+                                </div>
+                            `
+                            
+                            const address = address1 + " " + address2
+        
+                            const hotelData = {
+                                hotelName: name,
+                                address: address,
+                                image: img,
+                                user_id: userId
+                            }
+        
+                            console.log(hotelData)
+    
+                            fetch(booksURL, {
+                                method: "POST",
+                                headers: {
+                                    "Content-Type": "application/json",
+                                    "Accepts": "application/json"
+                                },
+                                body: JSON.stringify(hotelData)
+                            })
+                        })
                     }
-
-                    console.log(hotelData)
-
-
-                    // post to bookings
-
-                    
-                    fetch(booksURL, {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                            "Accepts": "application/json"
-                        },
-                        body: JSON.stringify(hotelData)
-                    })
                 })
-            }
+            })
+
+            const myBooksBtn = document.querySelector("#my-hotels")
+            
+            myBooksBtn.addEventListener("click", function(e){
+                // console.log("HI")
+
+                fetch(booksURL)
+                .then(json => json)
+                .then(data => {
+                    myHotel = data
+                    console.log(myHotel)
+
+                    titleContainer.innerHTML = `
+                        <h4>My Saved Hotels</h4>
+                    `
+
+                })
+            })
         })
-    })
-
-    const myBooksBtn = document.querySelector("#my-books")
-
-    myBooksBtn.addEventListener("click", function(e){
-        console.log("HI")
-    })
-
-    // fetch(booksURL)
-    // .then(json => json)
-    // .then(console.log)
-
+    }
+    renderUserForm()
     
 })
